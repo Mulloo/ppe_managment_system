ac1e378d8d3b19cd8f4d326e241ee4338e273ff2
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-01-31 19:09:11 +0000

    Add import new function inputs added to variables

--- run.py
+++ run.py
@@ -16,3 +16,22 @@ SHEET = GSPREAD_CLIENT.open("ppe_management_sheet")
 
 # ppe = SHEET.worksheet("in_use").get_all_values()
 # print(ppe)
+
+
+def import_new():
+    print(
+        "Please give the required information\n Name: \n Type: \n Code: \n Serial: \n Date of first use: \n Date of Manufacture:\n"
+    )
+    name = input("Name: ")
+    type = input("Type: ")
+    code = input("Code: ")
+    serial = input("Serial: ")
+    date_first_use = input("Date of first use dd/mm/yyyy: ")
+    date_of_manufacture = input("Date of Manufacture dd/mm/yyy: ")
+
+
+def main():
+    input
+
+
+import_new()
